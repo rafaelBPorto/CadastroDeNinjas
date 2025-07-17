503b60ce1e506d6d43aac28567e9e35620ac8c4e
rafaelbporto <rafaelbporto88@gmail.com>
2025-07-16 22:51:48 -0300

    fix: Adiciona a notation @Column(name) nos elemntos de NinjaModel

--- src/main/java/rafaelBPorto/com/github/CadastrosDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/rafaelBPorto/com/github/CadastrosDeNinjas/Ninjas/NinjaModel.java
@@ -1,21 +1,34 @@
 package rafaelBPorto.com.github.CadastrosDeNinjas.Ninjas;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import rafaelBPorto.com.github.CadastrosDeNinjas.Missoes.MissaoModel;
 
 import java.util.List;
 
 @Entity // Transforma a class em uma entidade do banco de dados
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private Long id;
 
     private String nome;
 
+    @Column (unique = true)
     private String email;
 
+    @Column (name = "img_url")
+    private String imaUrl;
+
+    @Column (name = "idade")
     private int idade;
 
     // Vários ninjas irão para uma única missão
@@ -23,36 +36,4 @@ public class NinjaModel {
     @JoinColumn(name = "missao_id") //FK
     private MissaoModel missao;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
