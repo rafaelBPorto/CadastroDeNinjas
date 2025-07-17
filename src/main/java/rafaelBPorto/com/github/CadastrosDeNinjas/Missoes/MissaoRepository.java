df13b47af52b4f3849111ed3ead69e8d33352b0c
rafaelbporto <rafaelbporto88@gmail.com>
2025-07-16 22:52:25 -0300

    feat: Criar a interface MissaoRepository

---
+++ src/main/java/rafaelBPorto/com/github/CadastrosDeNinjas/Missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package rafaelBPorto.com.github.CadastrosDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
