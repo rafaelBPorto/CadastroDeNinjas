e320924bc2bd91d6986480f8afca437e530dab60
rafaelbporto <rafaelbporto88@gmail.com>
2025-07-19 13:37:06 -0300

    feat: adiciona CRUD do NinjaController com mock

--- src/main/java/rafaelBPorto/com/github/CadastrosDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/rafaelBPorto/com/github/CadastrosDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package rafaelBPorto.com.github.CadastrosDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,38 @@ public class NinjaController {
     @GetMapping("/")
     public String boasVindas(){
         return "API para Cadastro de Ninjas";
+    }
+
+
+    /* CRUD */
+
+    // TODO Adicionar ninja (CREATE)
+    @PostMapping("/ninja")
+    public String criarNinja(){
+        return "Ninja Criado com Sucesso";
+    }
+
+    // TODO Procurar Ninja por ID (READ)
+    @GetMapping("/ninja/{id}")
+    public String encontrarNinja(@PathVariable int id){
+        return "Ninja " + id + " encontrado";
+    }
+
+    // TODO Mostrar todos os ninjas (READ)
+    @GetMapping("/ninja")
+    public String mostarNinjas(){
+        return "Ninjas existentes";
+    }
+
+    // TODO Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/ninja/{id}")
+    public String alterarNinja(@PathVariable int id){
+        return "Ninja alterado com Sucesso";
+    }
+
+    // TODO Deletar Ninja (DELETE)
+    @DeleteMapping("ninja/{id}")
+    public String deletarNinja(@PathVariable int id){
+        return "Ninja " + id + " apagado com Sucesso";
     }
 }
