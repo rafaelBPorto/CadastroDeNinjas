cf198ba8966a3fbe9ef8e8ee1bb2734035e44b2b
rafaelbporto <rafaelbporto88@gmail.com>
2025-07-22 10:35:35 -0300

    chore: inclui a versão do DTO pela entidade e mapeia manualemente no NinjaMapper os caminho da entidade vs DTO

--- src/main/java/rafaelBPorto/com/github/CadastrosDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/rafaelBPorto/com/github/CadastrosDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,19 @@
 package rafaelBPorto.com.github.CadastrosDeNinjas.Ninjas;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import rafaelBPorto.com.github.CadastrosDeNinjas.Missoes.MissaoModel;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissaoModel missao;
 }
